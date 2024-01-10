85668432b0f31fdf56f73c9fca4775f805996119
marufalaslam <maruf14@cse.pstu.ac.bd>
2024-01-11 03:39:26 +0600

    arrow controlled

--- js/app.js
+++ js/app.js
@@ -1,23 +1,33 @@
 let currentSection = 2
 
 $('.down-btn').click(function () {
-
+    $('.up-btn').css('display', 'block')
     if (currentSection < 6) {
         currentSection++
         $('.current').text(currentSection - 1)
         $(`section:nth-child(${currentSection - 1})`).css("transform", "translateY(-100%)").css("z-index", 100)
         $(`section:nth-child(${currentSection})`).css("transform", "translateY(0)").css("z-index", currentSection * 100)
     }
+
+    if (currentSection == 6) {
+        $('.down-btn').css('display', 'none')
+    }
 })
 
 $('.up-btn').click(function () {
 
     if (currentSection > 2) {
+        $('.up-btn').css('display', 'block')
         currentSection--
         $('.current').text(currentSection - 1)
         $(`section:nth-child(${currentSection})`).css("transform", "translateY(0)").css("z-index", currentSection * 100)
         $(`section:nth-child(${currentSection + 1})`).css("transform", "translateY(100%)").css("z-index", 100)
     }
+
+    if (currentSection === 2) {
+        $('.up-btn').css('display', 'none')
+    }
+
 })
 
 
